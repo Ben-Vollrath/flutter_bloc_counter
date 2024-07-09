abf94f80d7d835959b5eaec976eb479889992cf9
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-07-09 20:38:47 +0200

    Added main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+import 'app.dart';
+import 'counter_observer.dart';
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+void main() {
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
 }
